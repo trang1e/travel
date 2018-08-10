d76998f250e49b710d82aa1a7d5f949ff91038b6
trang1e <1461549400@qq.com>
2018-08-10 13:40:29 +0800

    更新-02

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import router from './router'
 import 'styles/reset.css'
 import fastClick from 'fastclick'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
